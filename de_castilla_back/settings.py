2b5fdd7fcd4d4bfe470f478fa0319eedebc7548c
Camila Arias <ariasruizcamilaa@gmail.com>
2024-02-06 14:29:29 -0500

    Archivos y modelos de la base de datos

--- de_castilla_back/settings.py
+++ de_castilla_back/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'de_castilla_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'db_de_castilla',
+        'PASSWORD': '',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
